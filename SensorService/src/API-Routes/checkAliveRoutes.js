02b28c22a12f58140ab7446cfcefde7a0a7d2307
hasan-kayan <your-email@example.com>
2025-01-09 08:30:15 +0300

    Clear

--- SensorService/src/API-Routes/checkAliveRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { checkAlive } = require('../controllers/checkAliveController');
-const checkSensorRegistered = require('../middleware/checkSensorRegistered');
-const router = express.Router();
-
-// Apply the middleware to routes that need sensor registration checks
-router.get('/check-alive', checkSensorRegistered, checkAlive);
-
-module.exports = router;
--- SensorService/src/API-Routes/registerSensorRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const { registerSensor } = require("../Controllers/RegisterSensorController");
-const router = express.Router();
-
-router.post("/register-sensor", registerSensor);
-
-module.exports = router;
